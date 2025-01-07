bb4fef69a816b9eeef5d6507ba73662b96606261
lshyun955 <lshyun955@gmail.com>
2025-01-07 20:40:42 +0900

    🔧 style: import 순서 관련 lint옵션 수정

--- eslint.config.js
+++ eslint.config.js
@@ -24,12 +24,12 @@ export default tseslint.config(
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
-      'react': reactPlugin,
+      react: reactPlugin,
       'react-hooks': reactHooksPlugin,
       'jsx-a11y': jsxA11yPlugin,
-      'import': importPlugin,
-      'sonarjs': sonarjsPlugin,
-      'prettier': prettier,
+      import: importPlugin,
+      sonarjs: sonarjsPlugin,
+      prettier: prettier,
     },
     settings: {
       react: {
@@ -50,15 +50,21 @@ export default tseslint.config(
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
       '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/consistent-type-imports': ['error', {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: true,
-      }],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: true,
+        },
+      ],
       '@typescript-eslint/naming-convention': [
         'error',
         {
@@ -84,18 +90,27 @@ export default tseslint.config(
       'react/prop-types': 'off',
       'react/jsx-props-no-spreading': 'warn',
       'react/jsx-no-useless-fragment': 'error',
-      'react/jsx-curly-brace-presence': ['error', { 
-        props: 'never', 
-        children: 'never' 
-      }],
-      'react/self-closing-comp': ['error', {
-        component: true,
-        html: true,
-      }],
-      'react/function-component-definition': ['error', {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      }],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        {
+          props: 'never',
+          children: 'never',
+        },
+      ],
+      'react/self-closing-comp': [
+        'error',
+        {
+          component: true,
+          html: true,
+        },
+      ],
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
 
@@ -108,7 +123,6 @@ export default tseslint.config(
             'internal',
             ['parent', 'sibling'],
             'index',
-            'style',
             'type',
             'object',
           ],
@@ -143,33 +157,42 @@ export default tseslint.config(
       'import/no-default-export': 'off',
 
       // FSD 아키텍처 관련 규칙
-      'import/no-internal-modules': ['error', {
-        allow: [
-          'shared/**',
-          'entities/*',
-          'features/*',
-          'widgets/*',
-          'pages/*',
-          '**/model/**',
-          '**/ui/**',
-          '**/lib/**',
-          '**/api/**',
-        ],
-      }],
+      'import/no-internal-modules': [
+        'error',
+        {
+          allow: [
+            'shared/**',
+            'entities/*',
+            'features/*',
+            'widgets/*',
+            'pages/*',
+            '**/model/**',
+            '**/ui/**',
+            '**/lib/**',
+            '**/api/**',
+          ],
+        },
+      ],
 
       // 일반적인 코드 스타일 규칙
-      'no-console': ['warn', { 
-        allow: ['warn', 'error'] 
-      }],
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
       'no-debugger': 'error',
       'no-alert': 'error',
       'prefer-const': 'error',
       'no-var': 'error',
-      'no-multiple-empty-lines': ['error', { 
-        max: 1,
-        maxEOF: 0,
-        maxBOF: 0,
-      }],
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 0,
+          maxBOF: 0,
+        },
+      ],
       'padding-line-between-statements': [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
@@ -178,10 +201,13 @@ export default tseslint.config(
       ],
 
       // JSX 접근성 규칙
-      'jsx-a11y/anchor-is-valid': ['error', {
-        components: ['Link'],
-        specialLink: ['to'],
-      }],
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['to'],
+        },
+      ],
       'jsx-a11y/click-events-have-key-events': 'error',
       'jsx-a11y/no-static-element-interactions': 'error',
 
@@ -192,5 +218,5 @@ export default tseslint.config(
     },
   },
   ...tseslint.configs.recommended,
-  ...prettier.configs.recommended,
+  ...prettier.configs.recommended
 );
